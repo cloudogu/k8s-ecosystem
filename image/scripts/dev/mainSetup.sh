c30b45eebb68fa7f5752aef7aaa1fa5e1e84e12b
Robert Auer <robert.auer@cloudogu.com>
2022-07-14 10:02:36 +0200

    Add necessary ^Cgrant pub key; Update k3s and longhorn to latest release

---
+++ image/scripts/dev/mainSetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo "Setting KUBECONFIG in /home/vagrant/.bashrc and /root/.bashrc..."
+echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /home/vagrant/.bashrc
+sudo sh -c 'echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /root/.bashrc'
+
+echo "Generating registry.cloudogu.com secret..."
+kubectl delete secret --namespace=ecosystem registry-cloudogu-com || true
+kubectl create secret docker-registry k8s-dogu-operator-docker-registry --namespace=ecosystem --docker-server=registry.cloudogu.com --docker-username="$4" --docker-email="$6" --docker-password="$5"
+kubectl create secret generic k8s-dogu-operator-dogu-registry --namespace=ecosystem --from-literal=username="$1" --from-literal=password="$2" --from-literal=endpoint="$3"

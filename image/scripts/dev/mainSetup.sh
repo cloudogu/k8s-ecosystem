f91d079a3ac9dd8f1276965ff4023cc741fbbe85
Robert Auer <robert.auer@cloudogu.com>
2022-07-26 11:29:27 +0200

    Prepare multi node deployment with correct node names

--- image/scripts/dev/mainSetup.sh
+++ image/scripts/dev/mainSetup.sh
@@ -7,6 +7,16 @@ echo "Setting KUBECONFIG in /home/vagrant/.bashrc and /root/.bashrc..."
 echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /home/vagrant/.bashrc
 sudo sh -c 'echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /root/.bashrc'
 
+echo "Checking cluster connection..."
+for (( i = 1; i <=10; i++ )); do
+    if ! kubectl get nodes > /dev/null; then
+        echo "Cluster not available yet (${i})"
+        sleep 2
+    else
+        break
+    fi
+done
+
 echo "Generating registry.cloudogu.com secret..."
 kubectl delete secret --namespace=ecosystem registry-cloudogu-com || true
 kubectl create secret docker-registry k8s-dogu-operator-docker-registry --namespace=ecosystem --docker-server=registry.cloudogu.com --docker-username="$4" --docker-email="$6" --docker-password="$5"

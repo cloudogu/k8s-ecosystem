e919379bcea8d58bb99818abc1aa1ac0856d59d6
Robert Auer <robert.auer@cloudogu.com>
2022-07-28 10:07:30 +0200

    Configure worker nodes via node config file

--- resources/usr/sbin/k3s-conf.sh
+++ resources/usr/sbin/k3s-conf.sh
@@ -5,49 +5,68 @@ set -o pipefail
 
 export NODE_CONFIG_FILE=/etc/ces/nodeconfig/k3sConfig.json
 export K3S_SYSTEMD_ENV_DIR=/etc/systemd/system
-export K3S_SYSTEMD_ENV_FILE="${K3S_SYSTEMD_ENV_DIR}/k3s.service.env"
-export K3S_SYSTEMD_SERVICE_FILE="${K3S_SYSTEMD_ENV_DIR}/k3s.service"
 
 function runUpdatek3sConfiguration() {
     local configHasChanged="false"
+
+    echo "Determining whether this is the main host or a worker..."
+    if ls /etc/systemd/system | grep agent > /dev/null; then
+        echo "This is a k3s agent/worker node"
+        export K3S_SERVICE_NAME=k3s-agent
+    else
+        echo "This is a k3s main node"
+        export K3S_SERVICE_NAME=k3s
+    fi
+    export K3S_SYSTEMD_ENV_FILE="${K3S_SYSTEMD_ENV_DIR}/${K3S_SERVICE_NAME}.service.env"
+    export K3S_SYSTEMD_SERVICE_FILE="${K3S_SYSTEMD_ENV_DIR}/${K3S_SERVICE_NAME}.service"
+
     echo "Getting hostname..."
     hostname=$(cat /etc/hostname)
     echo "Hostname is ${hostname}"
-    if [[ -e ${NODE_CONFIG_FILE} ]]; then
-        echo "Getting node-ip, node-external-ip and flannel-iface configurations for ${hostname} from ${NODE_CONFIG_FILE}..."
-        nodeIp=$(jq -r ".\"${hostname}\" | .\"node-ip\"" ${NODE_CONFIG_FILE})
-        nodeExternalIp=$(jq -r ".\"${hostname}\" | .\"node-external-ip\"" ${NODE_CONFIG_FILE})
-        flannelIface=$(jq -r ".\"${hostname}\" | .\"flannel-iface\"" ${NODE_CONFIG_FILE})
-        echo "nodeIp = ${nodeIp}, nodeExternalIp = ${nodeExternalIp}, flannelIface = ${flannelIface}"
-        echo "Checking if configuration has changed..."
-        if [[ $(nodeIPHasChanged "${nodeIp}" "${K3S_SYSTEMD_SERVICE_FILE}") == "true" ]]; then
-            configHasChanged="true"
-            echo "NodeIP has changed"
-            echo "Replacing node-ip in ${K3S_SYSTEMD_SERVICE_FILE} with ${nodeIp}..."
-            replaceNodeIpInK3sServiceFile "${K3S_SYSTEMD_SERVICE_FILE}" "${nodeIp}"
-        fi
-        if [[ $(nodeExternalIPHasChanged "${nodeExternalIp}" "${K3S_SYSTEMD_ENV_FILE}" "${K3S_SYSTEMD_SERVICE_FILE}") == "true" ]]; then
-            configHasChanged="true"
-            echo "NodeExternalIP has changed"
-            echo "Replacing node-external-ip in ${K3S_SYSTEMD_SERVICE_FILE} with ${nodeExternalIp}..."
-            replaceNodeExternalIpInK3sServiceFile "${K3S_SYSTEMD_SERVICE_FILE}" "${nodeExternalIp}"
+
+    echo "Getting node-ip, node-external-ip and flannel-iface configurations for ${hostname} from ${NODE_CONFIG_FILE}..."
+    nodeIp=$(jq -r ".\"${hostname}\" | .\"node-ip\"" ${NODE_CONFIG_FILE})
+    nodeExternalIp=$(jq -r ".\"${hostname}\" | .\"node-external-ip\"" ${NODE_CONFIG_FILE})
+    flannelIface=$(jq -r ".\"${hostname}\" | .\"flannel-iface\"" ${NODE_CONFIG_FILE})
+    echo "nodeIp = ${nodeIp}, nodeExternalIp = ${nodeExternalIp}, flannelIface = ${flannelIface}"
+
+    echo "Checking if configuration has changed..."
+    if [[ $(nodeIPHasChanged "${nodeIp}" "${K3S_SYSTEMD_SERVICE_FILE}") == "true" ]]; then
+        configHasChanged="true"
+        echo "NodeIP has changed"
+        echo "Replacing node-ip in ${K3S_SYSTEMD_SERVICE_FILE} with ${nodeIp}..."
+        replaceNodeIpInK3sServiceFile "${K3S_SYSTEMD_SERVICE_FILE}" "${nodeIp}"
+    fi
+    if [[ $(nodeExternalIPHasChanged "${nodeExternalIp}" "${K3S_SYSTEMD_ENV_FILE}" "${K3S_SYSTEMD_SERVICE_FILE}") == "true" ]]; then
+        configHasChanged="true"
+        echo "NodeExternalIP has changed"
+        echo "Replacing node-external-ip in ${K3S_SYSTEMD_SERVICE_FILE} with ${nodeExternalIp}..."
+        replaceNodeExternalIpInK3sServiceFile "${K3S_SYSTEMD_SERVICE_FILE}" "${nodeExternalIp}"
+        if [[ ${K3S_SERVICE_NAME} == "k3s" ]]; then
             echo "Replacing K3S_EXTERNAL_IP in ${K3S_SYSTEMD_ENV_FILE} with ${nodeExternalIp}..."
             replaceNodeExternalIpInK3sEnvFile "${K3S_SYSTEMD_ENV_FILE}" "${nodeExternalIp}"
         fi
-        if [[ $(flannelIfaceHasChanged "${flannelIface}" "${K3S_SYSTEMD_SERVICE_FILE}") == "true" ]]; then
+    fi
+    if [[ $(flannelIfaceHasChanged "${flannelIface}" "${K3S_SYSTEMD_SERVICE_FILE}") == "true" ]]; then
+        configHasChanged="true"
+        echo "FlannelIface has changed"
+        echo "Replacing flannel-iface in ${K3S_SYSTEMD_SERVICE_FILE} with ${flannelIface}"
+        replaceFlannelIfaceInK3sServiceFile "${K3S_SYSTEMD_SERVICE_FILE}" "${flannelIface}"
+    fi
+    if [[ ${K3S_SERVICE_NAME} == "k3s-agent" ]]; then
+        mainNodeIp=$(jq -r ".[] | select(.isMainNode==true) | .\"node-ip\"" ${NODE_CONFIG_FILE})
+        if [[ $(mainNodeIpHasChanged ${K3S_SYSTEMD_ENV_FILE} "${mainNodeIp}") == "true" ]]; then
             configHasChanged="true"
-            echo "FlannelIface has changed"
-            echo "Replacing flannel-iface in ${K3S_SYSTEMD_SERVICE_FILE} with ${flannelIface}"
-            replaceFlannelIfaceInK3sServiceFile "${K3S_SYSTEMD_SERVICE_FILE}" "${flannelIface}"
+            echo "Main node IP has changed"
+            echo "Replacing K3S_URL in ${K3S_SYSTEMD_ENV_FILE} with ${mainNodeIp}..."
+            replaceK3sUrlInK3sEnvFile "${K3S_SYSTEMD_ENV_FILE}" "${mainNodeIp}"
         fi
-        if [[ ${configHasChanged} == "true" ]]; then
-            reloadDaemon
-            restartK3s
-        else
-            echo "Configuration has not changed"
-        fi
+    fi
+    if [[ ${configHasChanged} == "true" ]]; then
+        reloadDaemon
+        restartK3s
     else
-        echo "Config file ${NODE_CONFIG_FILE} does not exist. Exiting."
+        echo "Configuration has not changed"
     fi
 }
 
@@ -57,8 +76,8 @@ function reloadDaemon() {
 }
 
 function restartK3s() {
-    echo "Restarting k3s service..."
-    systemctl restart k3s
+    echo "Restarting ${K3S_SERVICE_NAME} service..."
+    systemctl restart ${K3S_SERVICE_NAME}
 }
 
 function nodeIPHasChanged() {
@@ -72,6 +91,16 @@ function nodeIPHasChanged() {
     fi
 }
 
+function mainNodeIpHasChanged() {
+    local k3sEnvFile="${1}"
+    local mainNodeIp="${2}"
+
+    if grep "K3S_URL" "${k3sEnvFile}" | grep "${mainNodeIp}"; then
+        echo "false"
+    else
+        echo "true"
+    fi
+}
 
 function nodeExternalIPHasChanged() {
     local nodeExternalIp="${1}"
@@ -122,6 +151,12 @@ function replaceFlannelIfaceInK3sServiceFile() {
     local k3sServiceFile="${1}"
     local flannelIface="${2}"
     sed -i "s|\(--flannel-iface\)=.*$|\1=${flannelIface}' \\\|g" "${k3sServiceFile}"
+}
+
+function replaceK3sUrlInK3sEnvFile() {
+    local k3sEnvFile="${1}"
+    local mainNodeIp="${2}"
+    sed -i "s|^\(K3S_URL\)=.\+$|\1='https://${mainNodeIp}:6443'|g" "${k3sEnvFile}"
 }
 
 

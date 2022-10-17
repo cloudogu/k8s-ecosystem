0e9713e6c7fd29afe4986e6babe08c47cb6acd79
Robert Auer <robert.auer@cloudogu.com>
2022-10-17 15:01:15 +0200

    Install k9s to prod and dev images; #11

---
+++ image/scripts/installK9s.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+installDir=/usr/local/bin/
+
+echo "Installing k9s to ${installDir}..."
+wget -q https://github.com/derailed/k9s/releases/download/v0.26.6/k9s_Linux_x86_64.tar.gz
+mkdir -p "${installDir}"
+tar xf k9s_Linux_x86_64.tar.gz -C "${installDir}"
+
+echo "DEBUG Installed k9s!"
+whereis k9s
+echo "DEBUG: DONE"

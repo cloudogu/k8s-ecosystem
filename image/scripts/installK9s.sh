763315c347baae41aa3386c81a07cac999121d4f
Robert Auer <robert.auer@cloudogu.com>
2022-10-28 13:29:12 +0200

    Configure KUBECONFIG in .bashrc; #11

--- image/scripts/installK9s.sh
+++ image/scripts/installK9s.sh
@@ -9,3 +9,5 @@ echo "Installing k9s to ${installDir}..."
 wget -q https://github.com/derailed/k9s/releases/download/v0.26.6/k9s_Linux_x86_64.tar.gz
 mkdir -p "${installDir}"
 tar xf k9s_Linux_x86_64.tar.gz -C "${installDir}"
+echo "Configuring KUBECONFIG..."
+echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /home/"${USERNAME}"/.bashrc

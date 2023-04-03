d5f146c5c911067c97ee9dc9f818df10b72c04e2
Niklas <niklas.hinze@cloudogu.com>
2023-04-03 13:37:37 +0200

    #18 Use cloudogu/k8s-longhorn
    Use cloudogu/k8s-longhorn because of configuration f. e. node failure.

--- image/scripts/kubernetes/prepareK3sInstallation.sh
+++ image/scripts/kubernetes/prepareK3sInstallation.sh
@@ -4,7 +4,7 @@ set -o nounset
 set -o pipefail
 
 k3sVersion=v1.24.9+k3s1
-longhornVersion=v1.4.0
+longhornVersion=v1.4.1-1
 k3sBinarySHA256SUM=4dd997c611739fb5c540519492751bdf8751dfa9afbd331df6c18fd8f982efb4
 k3sImagesTarSHA256SUM=bfb5ae74056c41a8a1b7ebd3941b600135307d2892b073e0f58585e3e276aa63
 
@@ -21,7 +21,7 @@ wget -q "https://get.k3s.io/" -O install.sh
 chmod +x install.sh
 
 echo "Downloading longhorn deployment yaml..."
-wget -q https://raw.githubusercontent.com/longhorn/longhorn/${longhornVersion}/deploy/longhorn.yaml -O longhorn.yaml
+wget -q "https://raw.githubusercontent.com/cloudogu/k8s-longhorn/${longhornVersion}/manifests/longhorn.yaml" -O longhorn.yaml
 
 echo "Moving k3s binary to /usr/local/bin and making it executable..."
 mv k3s /usr/local/bin/k3s

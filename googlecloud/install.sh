d8b52e3d640d88eb83c3055c663a852c3a99c46f
Niklas <niklas.hinze@cloudogu.com>
2023-05-04 14:51:29 +0200

    #27 Do not install longhorn

--- googlecloud/install.sh
+++ googlecloud/install.sh
@@ -8,6 +8,6 @@ source .env.sh
 kubectl config set-context gke_ces-operations-internal_europe-west3-a_ces-multinode
 NAMESPACE="ecosystem"
 ./createNamespace.sh "${NAMESPACE}"
-./installLonghorn.sh
+#./installLonghorn.sh
 ./createCredentials.sh "${dogu_registry_username}" "${dogu_registry_password}" "${dogu_registry_url}" "${image_registry_username}" "${image_registry_password}" "${image_registry_email}"
 ./installLatestK8sCesSetup.sh "${NAMESPACE}"

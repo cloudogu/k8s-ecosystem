89a4dd5da4153c676089130a35770b1b65bf271d
Benjamin Ernst <benjamin.ernst@cloudogu.com>
2023-07-20 14:03:59 +0200

    #35 add custom vnet to azure-cluster

--- terraform/azure/azure_aks.tf
+++ terraform/azure/azure_aks.tf
@@ -11,6 +11,30 @@ resource "azurerm_resource_group" "default" {
   }
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                        = "${var.aks_cluster_name}-vnet"
+  location                    = azurerm_resource_group.default.location
+  resource_group_name         = azurerm_resource_group.default.name
+  address_space               = ["10.1.0.0/16"]
+  tags = {
+    environment = "CES"
+  }
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                        = "aks_subnet"
+  resource_group_name         = azurerm_resource_group.default.name
+  virtual_network_name        = azurerm_virtual_network.vnet.name
+  address_prefixes            = ["10.1.1.0/24"]
+}
+
+resource "azurerm_subnet" "jenkins_agents_subnet" {
+  name                        = "jenkins_agents_subnet"
+  resource_group_name         = azurerm_resource_group.default.name
+  virtual_network_name        = azurerm_virtual_network.vnet.name
+  address_prefixes            = ["10.1.2.0/24"]
+}
+
 resource "azurerm_kubernetes_cluster" "default" {
   name                = "${var.aks_cluster_name}-aks"
   location            = azurerm_resource_group.default.location
@@ -22,6 +46,7 @@ resource "azurerm_kubernetes_cluster" "default" {
     node_count      = var.aks_node_count
     vm_size         = var.aks_vm_size
     os_disk_size_gb = 30
+    vnet_subnet_id  = azurerm_subnet.aks_subnet.id
   }
 
   service_principal {

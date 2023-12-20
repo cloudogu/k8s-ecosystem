4a57018ec9edef7ae293137b703858d1969eb529
Robert Auer <robert.auer@cloudogu.com>
2023-12-20 11:37:32 +0100

    Use tar parameters in correct order

--- image/scripts/installHelm.sh
+++ image/scripts/installHelm.sh
@@ -11,5 +11,5 @@ echo "Installing helm to ${installDir}..."
 wget -q https://get.helm.sh/helm-${helmVersion}-linux-amd64.tar.gz
 echo "${helmTarSHA256SUM} helm-${helmVersion}-linux-amd64.tar.gz" | sha256sum --check
 mkdir -p "${installDir}"
-tar zxvf helm-${helmVersion}-linux-amd64.tar.gz --strip-components 1 linux-amd64/helm -C "${installDir}"
+tar -zxvf helm-${helmVersion}-linux-amd64.tar.gz --strip-components=1 -C "${installDir}" linux-amd64/helm
 rm helm-${helmVersion}-linux-amd64.tar.gz

dbba3dc6fe58ca947842afaf13508628af5b29ae
kahoona77 <benjamin.ernst@cloudogu.com>
2024-06-24 15:19:55 +0200

    Update terraform/kubelet-private-registry/variables.tf
    Co-authored-by: nhinze23 <83591279+nhinze23@users.noreply.github.com>

--- terraform/kubelet-private-registry/variables.tf
+++ terraform/kubelet-private-registry/variables.tf
@@ -1,5 +1,5 @@
 variable "private_registries" {
-  description = "A list of private container-registries. Each entry must habe an 'url', an 'username' and a 'password' (base64-encoded)"
+  description = "A list of private container-registries. Each entry must have an 'url', an 'username' and a 'password' (base64-encoded)"
   type = list(object({
     url = string
     username = string

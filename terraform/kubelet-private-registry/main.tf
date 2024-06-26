39d5244205c8582742ed8df6e3df620381c27a88
Niklas <niklas.hinze@cloudogu.com>
2024-06-26 14:21:29 +0200

    #87 Use hcl daemonsets because the manifest resource needs the kubeconfig already in plan phase.

--- terraform/kubelet-private-registry/main.tf
+++ terraform/kubelet-private-registry/main.tf
@@ -30,6 +30,74 @@ resource "kubernetes_secret" "kubelet-private-registry-secret" {
   }
 }
 
-resource "kubernetes_manifest" "kubelet-private-registry-daemonset" {
-  manifest = yamldecode(file("${path.module}/manifests/kubelet_private_registry-daemonset.yaml"))
+resource "kubernetes_daemonset" "registry_config" {
+  metadata {
+    name      = "registry-config"
+    namespace = "kube-system"
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        name = "registry-config"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          name = "registry-config"
+        }
+      }
+
+      spec {
+        volume {
+          name = "host-root"
+
+          host_path {
+            path = "/"
+          }
+        }
+
+        volume {
+          name = "config-volume"
+
+          secret {
+            secret_name = "kubelet-config"
+          }
+        }
+
+        container {
+          name    = "update-kublet-config"
+          image   = "alpine:3.20.0"
+          command = ["/bin/sh", "-c"]
+          args    = ["while true; do cp -v /config/config.json /host/var/lib/kubelet/config.json; sleep 3600; done"]
+
+          volume_mount {
+            name       = "host-root"
+            mount_path = "/host"
+          }
+
+          volume_mount {
+            name       = "config-volume"
+            mount_path = "/config"
+          }
+        }
+
+        host_network = true
+        host_pid     = true
+        host_ipc     = true
+
+        toleration {
+          operator = "Exists"
+          effect   = "NoExecute"
+        }
+
+        toleration {
+          operator = "Exists"
+          effect   = "NoSchedule"
+        }
+      }
+    }
+  }
 }
--- terraform/max-map-count/main.tf
+++ terraform/max-map-count/main.tf
@@ -7,8 +7,59 @@ terraform {
   }
 }
 
-resource "kubernetes_manifest" "increase-max-map-count-daemonset" {
-  manifest = yamldecode(templatefile("${path.module}/manifests/max_map_count.yaml.tpl", {
-    "node_selectors" = var.node_selectors,
-  }))
+resource "kubernetes_daemonset" "max_map_count" {
+  metadata {
+    name      = "max-map-count"
+    namespace = "kube-system"
+
+    labels = {
+      app = "max-map-count"
+    }
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        app = "max-map-count"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "max-map-count"
+        }
+      }
+
+      spec {
+        node_selector = var.node_selectors
+
+        container {
+          name              = "sysctl-set"
+          image             = "alpine:3.20.0"
+          command           = ["/bin/sh", "-c"]
+          args              = ["while true; do sysctl -w vm.max_map_count=262144; sleep 3600; done"]
+          image_pull_policy = "IfNotPresent"
+
+          security_context {
+            privileged = true
+          }
+        }
+
+        host_network = true
+        host_pid     = true
+        host_ipc     = true
+
+        toleration {
+          operator = "Exists"
+          effect   = "NoExecute"
+        }
+
+        toleration {
+          operator = "Exists"
+          effect   = "NoSchedule"
+        }
+      }
+    }
+  }
 }

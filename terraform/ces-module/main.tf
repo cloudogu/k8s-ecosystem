e75b44f3904be5b93845605154e602109ca5895f
meiserloh <matthias.eiserloh@cloudogu.com>
2025-01-16 15:21:18 +0100

    #105 add ability to configure cas oidc authonticator with terraform

--- terraform/ces-module/main.tf
+++ terraform/ces-module/main.tf
@@ -16,20 +16,23 @@ terraform {
 locals {
   split_fqdn = split(".", var.ces_fqdn)
   # Top Level Domain extracted from fully qualified domain name. k3ces.local is used for development mode and empty fqdn.
-  topLevelDomain = var.ces_fqdn != "" ? "${element( split(".", var.ces_fqdn), length(local.split_fqdn) - 2)}.${element(local.split_fqdn, length(local.split_fqdn) - 1)}" : "k3ces.local"
+  topLevelDomain = var.ces_fqdn != "" ?
+    "${element( split(".", var.ces_fqdn), length(local.split_fqdn) - 2)}.${element(local.split_fqdn, length(local.split_fqdn) - 1)}"
+    : "k3ces.local"
   splitComponentNamespaces = [
     for componentStr in var.components :
     {
       namespace = split("/", componentStr)[0]
-      rest      = split("/", componentStr)[1] //provoke error here, so that the build fails if no namespace or name is given
+      rest      = split("/", componentStr)[1]
+      //provoke error here, so that the build fails if no namespace or name is given
     }
   ]
   parsedComponents = [
     for namespaceAndRest in local.splitComponentNamespaces :
     {
-      namespace = namespaceAndRest.namespace
-      name      = split(":", namespaceAndRest.rest)[0]
-      version   = length(split(":", namespaceAndRest.rest)) == 2 ? split(":", namespaceAndRest.rest)[1] : "latest"
+      namespace       = namespaceAndRest.namespace
+      name            = split(":", namespaceAndRest.rest)[0]
+      version         = length(split(":", namespaceAndRest.rest)) == 2 ? split(":", namespaceAndRest.rest)[1] : "latest"
       deployNamespace = split(":", namespaceAndRest.rest)[0] != "k8s-longhorn" ? var.ces_namespace : "longhorn-system"
     }
   ]
@@ -61,7 +64,7 @@ resource "helm_release" "k8s-ces-setup" {
         "component_operator_chart"     = var.component_operator_chart
         "component_operator_crd_chart" = var.component_operator_crd_chart
         "components"                   = local.parsedComponents
-        "setup_json"                   = yamlencode(templatefile(
+        "setup_json" = yamlencode(templatefile(
           "${path.module}/setup.json.tftpl",
           {
             # https://docs.cloudogu.com/en/docs/system-components/ces-setup/operations/setup-json/
@@ -73,8 +76,22 @@ resource "helm_release" "k8s-ces-setup" {
             "fqdn"            = var.ces_fqdn
             "domain"          = local.topLevelDomain
             "certificateType" = var.ces_certificate_path == null ? "selfsigned" : "external"
-            "certificate"     = var.ces_certificate_path != null ? replace(file(var.ces_certificate_path), "\n", "\\n") : ""
-            "certificateKey"  = var.ces_certificate_key_path != null ? replace(file(var.ces_certificate_key_path), "\n", "\\n") : ""
+            "certificate"     = var.ces_certificate_path != null ? replace(file(var.ces_certificate_path), "\n", "\\n")
+              : ""
+            "certificateKey" = var.ces_certificate_key_path != null ?
+              replace(file(var.ces_certificate_key_path), "\n", "\\n") : ""
+            # Cas OIDC config values
+            "cas_oidc_enabled"= var.cas_oidc_enabled
+            "cas_oidc_discovery_uri"= var.cas_oidc_discovery_uri
+            "cas_oidc_client_id"= var.cas_oidc_client_id
+            "cas_oidc_client_secret"= var.cas_oidc_client_secret
+            "cas_oidc_display_name"= var.cas_oidc_display_name
+            "cas_oidc_optional"= var.cas_oidc_optional
+            "cas_oidc_scopes"= var.cas_oidc_scopes
+            "cas_oidc_attribute_mapping"= var.cas_oidc_attribute_mapping
+            "cas_oidc_principal_attribute"= var.cas_oidc_principal_attribute
+            "cas_oidc_allowed_groups"= var.cas_oidc_allowed_groups
+            "cas_oidc_initial_admin_usernames"= var.cas_oidc_initial_admin_usernames
           }
         ))
         "resource_patches" = var.resource_patches

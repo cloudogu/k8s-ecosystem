98165897182dc3bd2a77f429dcdcdbcb222b756a
Marco Bergen <marco.bergen-extern@cloudogu.com>
2025-10-28 08:52:24 +0000

    use current blueprint v2 and dependency versions

--- terraform/ces-module/variables.tf
+++ terraform/ces-module/variables.tf
@@ -75,7 +75,7 @@ variable "components" {
   })
   default = {
     components = [
-      { namespace = "ecosystem", name = "k8s-dogu-operator-crd", version = "2.9.0" },
+      { namespace = "ecosystem", name = "k8s-dogu-operator-crd", version = "2.10.0" },
       { namespace = "ecosystem", name = "k8s-dogu-operator", version = "3.13.0" },
       { namespace = "ecosystem", name = "k8s-service-discovery", version = "3.0.0" },
       { namespace = "ecosystem", name = "k8s-blueprint-operator-crd", version = "1.3.0", disabled = true},

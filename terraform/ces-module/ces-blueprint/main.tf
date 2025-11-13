c91bce416b7a99c0678b04e0e4d7e4fc65751f68
Nico Franzeck <nico.franzeck@cloudogu.com>
2025-11-13 10:00:36 +0100

    only set fqdn in blueprint when it is not empty.

--- terraform/ces-module/ces-blueprint/main.tf
+++ terraform/ces-module/ces-blueprint/main.tf
@@ -1,28 +1,34 @@
 locals {
+  has_ces_fqdn = trimspace(var.ces_fqdn) != ""
+
   split_fqdn = split(".", var.ces_fqdn)
   # Top Level Domain extracted from fully qualified domain name. k3ces.local is used for development mode and empty fqdn.
   topLevelDomain = var.ces_fqdn != "" ? "${element(split(".", var.ces_fqdn), length(local.split_fqdn) - 2)}.${element(local.split_fqdn, length(local.split_fqdn) - 1)}" : "k3ces.local"
 
-  globalConfig = [
-    # Naming
-    { key = "fqdn", value = var.ces_fqdn },
-    { key = "domain", value = local.topLevelDomain },
-    { key = "certificate/type", value = var.ces_certificate_path == null ? "selfsigned" : "external" },
-    # This must be added to secret: ecosystem-certificate
-    #{ key = "certificate", value = var.ces_certificate_path != null ? replace(file(var.ces_certificate_path), "\n", "\\n") : ""},
-    #{ key = "certificateKey", value = var.ces_certificate_key_path != null ? replace(file(var.ces_certificate_key_path), "\n", "\\n") : ""},
-    { key = "k8s/use_internal_ip", value = "false"},
-    { key = "internalIp", value = ""},
+  globalConfig = concat(
+    local.has_ces_fqdn ? [
+      { key = "fqdn", value = var.ces_fqdn },
+      { key = "domain", value = local.topLevelDomain }
+    ] : [],
 
-    { key = "password-policy/min_length", value: "1"} ,
-    { key = "password-policy/must_contain_capital_letter", value: "false"},
-    { key = "password-policy/must_contain_digit", value: "false" },
-    { key = "password-policy/must_contain_lower_case_letter", value = "false" },
-    { key = "password-policy/must_contain_special_character", value: "false" },
+    # Always-present entries
+    [
+      { key = "certificate/type", value = var.ces_certificate_path == null ? "selfsigned" : "external" },
+      # This must be added to secret: ecosystem-certificate
+      #{ key = "certificate", value = var.ces_certificate_path != null ? replace(file(var.ces_certificate_path), "\n", "\\n") : ""},
+      #{ key = "certificateKey", value = var.ces_certificate_key_path != null ? replace(file(var.ces_certificate_key_path), "\n", "\\n") : ""},
 
-    # Admin
-    { key = "admin_group", value = "cesAdmin"},
-  ]
+      # Password Policy
+      {key  = "password-policy/min_length", value: "1"} ,
+      { key = "password-policy/must_contain_capital_letter", value: "false"},
+      { key = "password-policy/must_contain_digit", value: "false" },
+      { key = "password-policy/must_contain_lower_case_letter", value = "false" },
+      { key = "password-policy/must_contain_special_character", value: "false" },
+
+      # Admin
+      { key = "admin_group", value = "cesAdmin"},
+    ],
+  )
 }
 
 # The Blueprint is used to configure the system after the ecosystem-core has installed all

d38c1b6112c85de63abf00d6f7ed1c0143aa67c4
Marco Bergen <marco.bergen-extern@cloudogu.com>
2026-04-08 15:40:14 +0200

    remove loadbalancer patch from terraform module

--- terraform/ces-module/ces-core/loadbalancer.tf
+++ terraform/ces-module/ces-core/loadbalancer.tf
@@ -12,17 +12,3 @@ data "kubernetes_service" "ces_lb_exists" {
   }
   depends_on = [helm_release.ecosystem-core]
 }
-
-# patch loadbalancer-service "ces-loadbalancer"
-resource "kubectl_manifest" "ces_loadbalancer_ip_patch" {
-  yaml_body = templatefile(
-    "${path.module}/loadbalancer.yaml.tftpl",
-    {
-      "externalIP"    = local.ext_ip,
-      "ces_namespace" = var.ces_namespace
-    })
-
-  server_side_apply = true
-
-  depends_on        = [data.kubernetes_service.ces_lb_exists]
-}

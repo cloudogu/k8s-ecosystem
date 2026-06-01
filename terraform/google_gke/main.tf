d758dc3a2177e62fbfbfc5335cf10c09cdf0ac08
Leon <leon-lukas.michelson@cloudogu.com>
2026-06-01 09:13:53 +0200

    Removed unneccesary comment

--- terraform/google_gke/main.tf
+++ terraform/google_gke/main.tf
@@ -27,8 +27,7 @@ resource "google_container_cluster" "default" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  // activate Dataplane 2, so that cilium can be used for network policies
-  // FIXME: currently, mn-ces does not support cilium as network policies seem to be more restrictive there
+  // activate Dataplane 2, so that cilium can be used to enforce network policies
   datapath_provider                        = "ADVANCED_DATAPATH"
   enable_cilium_clusterwide_network_policy = true
   cost_management_config {
